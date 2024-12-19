84173c169f9f3e379a575a6308047eea87b20f68
Naheed <89731453+summandee@users.noreply.github.com>
2024-12-19 01:11:07 -0800

    Update Assignment_01 edit grade

--- Assignment_01.py
+++ Assignment_01.py
@@ -48,6 +48,7 @@ print(salaries)
     
     
 #  10. Reverse the order of the salaries list without method.
+grade up salries
 salaries = salaries[::-1]
 print(salaries)
      ##  output    ['40k', '89k', '35k', '60k', '50k', '40k', '20k']
